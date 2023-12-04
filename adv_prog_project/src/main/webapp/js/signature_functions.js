d16bb28266e18b285fb88698aa8681d1b9bfb579
lallo-unitn <riccardo.gennaro@studenti.unitn.it>
2023-12-04 09:59:42 +0100

    importKey() in Web Crypto API requires TLS. Branching...

--- adv_prog_project/src/main/webapp/js/signature_functions.js
+++ adv_prog_project/src/main/webapp/js/signature_functions.js
@@ -15,6 +15,46 @@ function init() {
         })
 }
 
+function str2ab(str) {
+    const buf = new ArrayBuffer(str.length);
+    const bufView = new Uint8Array(buf);
+    for (let i = 0, strLen = str.length; i < strLen; i++) {
+        bufView[i] = str.charCodeAt(i);
+    }
+    return buf;
+}
+
+function importPrivateKey(): Promise<CryptoKey> {
+    const privateKeyPEM = localStorage.getItem("privateKey");
+    if (!privateKeyPEM) {
+        console.error("Private key not found in local storage");
+        return;
+    }
+    const pem = privateKeyPEM.replace(/\n/g, '');
+    const pemHeader = "-----BEGIN EC PRIVATE KEY-----";
+    const pemFooter = "-----END EC PRIVATE KEY-----";
+    const pemContents = pem.substring(
+        pemHeader.length,
+        pem.length - pemFooter.length,
+    );
+    // base64 decode the string to get the binary data
+    const binaryDerString = window.atob(pemContents);
+    // convert from a binary string to an ArrayBuffer
+    const binaryDer = str2ab(binaryDerString);
+
+    // Import private key from openssl format to WebCrypto format
+    return window.crypto.subtle.importKey(
+        "pkcs8",
+        binaryDer,
+        {
+            name: "ECDSA",
+            hash: "SHA-256",
+        },
+        true,
+        ["sign"],
+    );
+}
+
 async function sendGrade() {
     // Get user input values
     const studentId = document.getElementById("studentId").value;
@@ -24,39 +64,11 @@ async function sendGrade() {
         studentId: studentId,
         grade: grade,
     };
-    const privateKeyPEM = localStorage.getItem("privateKey");
-    if (!privateKeyPEM) {
-        console.error("Private key not found in local storage");
-        return;
-    }
-    const cleanedBase64 = privateKeyPEM.replace(/\n/g, '');
-    const match = cleanedBase64.match(/-----BEGIN EC PRIVATE KEY-----(.+)-----END EC PRIVATE KEY-----/s);
-    console.log(match);
-    let decodedPrivateKey
-    if (match && match[1]) {
-        const base64Encoded = match[1].trim();
-        console.log(base64Encoded);
-        // Decode the Base64-encoded string
-        decodedPrivateKey = atob(base64Encoded);
-        console.log(decodedPrivateKey);
-    } else {
-        console.error("Invalid base64 key format");
-    }
-
-    // Import private key from openssl format to WebCrypto format
-    const privateKey = await crypto.subtle.importKey(
-        "pkcs8",
-        new TextEncoder().encode(decodedPrivateKey).buffer,
-        {
-            name: "ECDSA",
-            namedCurve: "P-256",
-        },
-        false,
-        ["sign"]
-    );
     const jsonDataBytes = new TextEncoder().encode(
             JSON.stringify(jsonData)
         );
+    // Import private key
+    const privateKey = await importPrivateKey();
     // Sign data
     const signature = await crypto.subtle.sign(
         {
--- adv_prog_project/target/adv_prog_project-1.0-SNAPSHOT/js/signature_functions.js
+++ adv_prog_project/target/adv_prog_project-1.0-SNAPSHOT/js/signature_functions.js
@@ -15,6 +15,15 @@ function init() {
         })
 }
 
+function str2ab(str) {
+    const buf = new ArrayBuffer(str.length);
+    const bufView = new Uint8Array(buf);
+    for (let i = 0, strLen = str.length; i < strLen; i++) {
+        bufView[i] = str.charCodeAt(i);
+    }
+    return buf;
+}
+
 async function sendGrade() {
     // Get user input values
     const studentId = document.getElementById("studentId").value;
@@ -29,29 +38,27 @@ async function sendGrade() {
         console.error("Private key not found in local storage");
         return;
     }
-    const cleanedBase64 = privateKeyPEM.replace(/\n/g, '');
-    const match = cleanedBase64.match(/-----BEGIN EC PRIVATE KEY-----(.+)-----END EC PRIVATE KEY-----/s);
-    console.log(match);
-    let decodedPrivateKey
-    if (match && match[1]) {
-        const base64Encoded = match[1].trim();
-        console.log(base64Encoded);
-        // Decode the Base64-encoded string
-        decodedPrivateKey = atob(base64Encoded);
-        console.log(decodedPrivateKey);
-    } else {
-        console.error("Invalid base64 key format");
-    }
+    const pem = privateKeyPEM.replace(/\n/g, '');
+    const pemHeader = "-----BEGIN EC PRIVATE KEY-----";
+    const pemFooter = "-----END EC PRIVATE KEY-----";
+    const pemContents = pem.substring(
+        pemHeader.length,
+        pem.length - pemFooter.length,
+    );
+    // base64 decode the string to get the binary data
+    const binaryDerString = window.atob(pemContents);
+    // convert from a binary string to an ArrayBuffer
+    const binaryDer = str2ab(binaryDerString);
 
     // Import private key from openssl format to WebCrypto format
     const privateKey = await crypto.subtle.importKey(
         "pkcs8",
-        new TextEncoder().encode(decodedPrivateKey).buffer,
+        binaryDer,
         {
             name: "ECDSA",
             namedCurve: "P-256",
         },
-        false,
+        true,
         ["sign"]
     );
     const jsonDataBytes = new TextEncoder().encode(
