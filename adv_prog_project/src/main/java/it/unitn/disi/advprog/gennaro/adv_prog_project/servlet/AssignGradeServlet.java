e66bde4fe4f9447643cb44d3c982cc7f770fd397
lallo-unitn <riccardo.gennaro@studenti.unitn.it>
2023-12-04 09:19:00 +0100

    Implemented private key base64 decoding. Now implementing import for javascript Crypto API

--- adv_prog_project/src/main/java/it/unitn/disi/advprog/gennaro/adv_prog_project/servlet/AssignGradeServlet.java
+++ adv_prog_project/src/main/java/it/unitn/disi/advprog/gennaro/adv_prog_project/servlet/AssignGradeServlet.java
@@ -1,5 +1,7 @@
 package it.unitn.disi.advprog.gennaro.adv_prog_project.servlet;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
 import it.unitn.disi.advprog.gennaro.adv_prog_project.dto.TeacherDto;
 import it.unitn.disi.advprog.gennaro.adv_prog_project.managers.CourseManagerBean;
 import it.unitn.disi.advprog.gennaro.adv_prog_project.managers.StudentManagerBean;
@@ -10,7 +12,9 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.util.Base64;
 import java.util.List;
 
 public class AssignGradeServlet extends HttpServlet {
@@ -19,36 +23,90 @@ public class AssignGradeServlet extends HttpServlet {
     private TeacherManagerBean teacherManagerBean;
     @EJB
     private StudentManagerBean studentManagerBean;
+//    @Override
+//    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+//        // Get the studentId number from the request
+//        int studentId = Integer.parseInt(req.getParameter("studentId"));
+//
+//        // Get the studentDto attribute from the session
+//        TeacherDto teacherDto = (TeacherDto) req.getSession().getAttribute("teacherDto");
+//
+//        /*// Get the list of teachers associated with the student using the TeacherManagerBean
+//        List<TeacherDto> teacherList = teacherManagerBean.getTeacherByStudent(studentId);
+//
+//        // Check if the student is enrolled in any course
+//        if (!teacherList.contains(teacherDto)) {
+//            // Set an error message and forward to teacher.jsp
+//            req.setAttribute("messageStudent", "Not enrolled in your course");
+//            req.getRequestDispatcher("restricted/teacher.jsp").forward(req, resp);
+//            return;
+//        }*/
+//
+//        // Get the grade from the request
+//        int grade = Integer.parseInt(req.getParameter("grade"));
+//
+//        // Set the grade for the student using the TeacherManagerBean
+//        teacherManagerBean.setStudentGrade(
+//                studentManagerBean.getStudent(studentId),
+//                teacherDto.getCourse(),
+//                grade
+//        );
+//
+//        // Forward the request to teacher.jsp
+//        req.getRequestDispatcher("restricted/teacher.jsp").forward(req, resp);
+//    }
     @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        // Get the studentId number from the request
-        int studentId = Integer.parseInt(req.getParameter("studentId"));
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        // Read JSON data from the request body
+        StringBuilder requestBody = new StringBuilder();
+        try (BufferedReader reader = request.getReader()) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                requestBody.append(line);
+            }
+        }
 
-        // Get the studentDto attribute from the session
-        TeacherDto teacherDto = (TeacherDto) req.getSession().getAttribute("teacherDto");
+        // Parse JSON data using Gson library
+        Gson gson = new Gson();
+        JsonObject jsonData = gson.fromJson(requestBody.toString(), JsonObject.class);
 
-        /*// Get the list of teachers associated with the student using the TeacherManagerBean
-        List<TeacherDto> teacherList = teacherManagerBean.getTeacherByStudent(studentId);
+        // print json data
+        System.out.println(jsonData);
 
-        // Check if the student is enrolled in any course
-        if (!teacherList.contains(teacherDto)) {
-            // Set an error message and forward to teacher.jsp
-            req.setAttribute("messageStudent", "Not enrolled in your course");
-            req.getRequestDispatcher("restricted/teacher.jsp").forward(req, resp);
-            return;
-        }*/
+    //    // Extract fields from JSON
+    //    long timestamp = jsonData.get("timestamp").getAsLong();
+    //    String studentId = jsonData.get("studentId").getAsString();
+    //    String grade = jsonData.get("grade").getAsString();
+    //    String signatureBase64 = jsonData.get("signature").getAsString();
+    //
+    //    // Verify the signature (example: using Base64-decoded signature)
+    //    byte[] signature = Base64.getDecoder().decode(signatureBase64);
+    //    boolean signatureValid = verifySignature(jsonData, signature);
+    //
+    //    // Respond based on signature verification result
+    //    if (signatureValid) {
+    //        // Signature is valid, process the data (e.g., save to a database)
+    //        processValidData(timestamp, studentId, grade);
+    //
+    //        // Respond with a success message
+    //        sendResponse(response, "Signature verified, data processed successfully");
+    //    } else {
+    //        // Signature is not valid, reject the data
+    //        sendResponse(response, "Invalid signature, data rejected");
+    //    }
+    }
 
-        // Get the grade from the request
-        int grade = Integer.parseInt(req.getParameter("grade"));
+    private boolean verifySignature(JsonObject jsonData, byte[] signature) {
 
-        // Set the grade for the student using the TeacherManagerBean
-        teacherManagerBean.setStudentGrade(
-                studentManagerBean.getStudent(studentId),
-                teacherDto.getCourse(),
-                grade
-        );
+        return true;
+    }
 
-        // Forward the request to teacher.jsp
-        req.getRequestDispatcher("restricted/teacher.jsp").forward(req, resp);
+    private void processValidData(long timestamp, String studentId, String grade) {
+        // Implement data processing logic (e.g., save to a database)
+        System.out.println("Processing data - Timestamp: " + timestamp + ", Student ID: " + studentId + ", Grade: " + grade);
+    }
+
+    private void sendResponse(HttpServletResponse response, String message) throws IOException {
+        throw new UnsupportedOperationException("Not supported yet.");
     }
 }
--- adv_prog_project/src/main/webapp/js/signature_functions.js
+++ adv_prog_project/src/main/webapp/js/signature_functions.js
@@ -6,18 +6,74 @@ function init() {
             fr.onload = function () {
                 document.getElementById('output')
                     .textContent = fr.result;
-                localStorage.setItem('privKey', fr.result);
+                localStorage.setItem('privateKey', fr.result);
                 document.getElementById('outputKey')
-                    .textContent = localStorage.getItem('privKey');
+                    .textContent = localStorage.getItem('privateKey');
             }
 
             fr.readAsText(this.files[0]);
         })
 }
 
-function sendGrade(){
+async function sendGrade() {
     // Get user input values
     const studentId = document.getElementById("studentId").value;
     const grade = document.getElementById("grade").value;
-    const eccPrivKey = localStorage.getItem('privKey');
+    const jsonData = {
+        timestamp: Date.now(),
+        studentId: studentId,
+        grade: grade,
+    };
+    const privateKeyPEM = localStorage.getItem("privateKey");
+    if (!privateKeyPEM) {
+        console.error("Private key not found in local storage");
+        return;
+    }
+    const cleanedBase64 = privateKeyPEM.replace(/\n/g, '');
+    const match = cleanedBase64.match(/-----BEGIN EC PRIVATE KEY-----(.+)-----END EC PRIVATE KEY-----/s);
+    console.log(match);
+    let decodedPrivateKey
+    if (match && match[1]) {
+        const base64Encoded = match[1].trim();
+        console.log(base64Encoded);
+        // Decode the Base64-encoded string
+        decodedPrivateKey = atob(base64Encoded);
+        console.log(decodedPrivateKey);
+    } else {
+        console.error("Invalid base64 key format");
+    }
+
+    // Import private key from openssl format to WebCrypto format
+    const privateKey = await crypto.subtle.importKey(
+        "pkcs8",
+        new TextEncoder().encode(decodedPrivateKey).buffer,
+        {
+            name: "ECDSA",
+            namedCurve: "P-256",
+        },
+        false,
+        ["sign"]
+    );
+    const jsonDataBytes = new TextEncoder().encode(
+            JSON.stringify(jsonData)
+        );
+    // Sign data
+    const signature = await crypto.subtle.sign(
+        {
+            name: "ECDSA",
+            hash: { name: "SHA-256" },
+        },
+        privateKey,
+        jsonDataBytes
+    );
+    jsonData.signature = new Uint8Array(signature);
+    const response = await fetch("/AssignGradeServlet", {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(jsonData),
+    });
+    const responseData = await response.json();
+    console.log('Server Response:', responseData);
 }
